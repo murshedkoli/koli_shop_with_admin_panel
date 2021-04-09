187abc063323d440e51922fd9f0b4085345c1cd9
murshedkoli <38584905+murshedkoli@users.noreply.github.com>
2021-04-09 20:13:02 +0600

    Update Shipment.js

--- src/Component/Shipment/Shipment.js
+++ src/Component/Shipment/Shipment.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useContext } from 'react';
 import { useForm } from 'react-hook-form';
 import { useHistory } from 'react-router';
 import { mainUser } from '../../App';
